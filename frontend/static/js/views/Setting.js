1337cf5d345835feb044cc457dda8842618c30e8
Hazim Mazley <hazimmazley@gmail.com>
2022-06-11 23:03:19 +0800

    first commit

---
+++ frontend/static/js/views/Setting.js
@@ -0,0 +1,12 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor() {
+        super();
+        this.setTitle('Settings')
+    }
+
+    async getHtml() {
+        return `<h1>This is settings</h1>`;
+    }
+} 
